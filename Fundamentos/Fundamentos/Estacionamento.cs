55baa4e5384b6cb052cffa88623463a0ccd34475
Artur D Angelo <arturdangelo@gmail.com>
2023-07-05 22:22:49 -0300

    Desafio Fundamentos

---
+++ Fundamentos/Fundamentos/Estacionamento.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos
+{
+    public class Estacionamento
+    {
+        decimal precoInicial = 5.00M;
+        decimal precoPorHora = 3.00M;
+        List<string> veiculos = new List<string>();
+        string placa = null;
+        int horas = 0;
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Informe a placa do veículo:");
+            placa = Console.ReadLine();
+            veiculos.Add(placa);
+            Console.WriteLine();
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Informe a placa do veículo para remover:");
+            placa = Console.ReadLine();
+            Console.WriteLine();
+
+            string veiculoRemovido = veiculos.FirstOrDefault(x => x == placa);
+
+            if (veiculoRemovido != null)
+            {
+                Console.WriteLine("Informe a quantidade de horas");
+                horas = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Valor final: R$ {(precoInicial + (precoPorHora * horas))}");
+
+                veiculos.Remove(veiculoRemovido);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Veículo não encontrado.");
+                Console.WriteLine();
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            foreach (string v in veiculos)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Fundamentos/Fundamentos/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Fundamentos;
+
+internal class Program
+{
+    static void Main()
+    {
+        int opcao;
+        Estacionamento Veiculo = new Estacionamento();
+
+        do
+        {
+            Console.WriteLine("1. Cadastrar Veículo.\n" +
+                              "2. Remover Veículo\n" +
+                              "3. Listar Veículo\n" +
+                              "4. Encerrar");
+            Console.WriteLine();
+
+            opcao = int.Parse(Console.ReadLine());
+            switch (opcao)
+            {
+                case 1:
+                    Veiculo.AdicionarVeiculo();
+                    break;
+
+                case 2:
+                    Veiculo.RemoverVeiculo();
+                    break;
+
+                case 3:
+                    Veiculo.ListarVeiculos();
+                    break;
+
+                case 4:
+                    Console.WriteLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        } while (opcao != 4);
+    }
+}
